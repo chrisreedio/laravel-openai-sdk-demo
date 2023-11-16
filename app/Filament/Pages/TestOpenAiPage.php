a3b0a0888047631fa30f20b8bf2dd544684955c6
Chris Reed <chris@reedtech.us>
2023-11-15 19:40:48 -0600

    Add TestOpenAiPage with Fetch Models action
    This commit introduces a new Filament page, TestOpenAiPage, which includes a 'Fetch Models' action. This action is currently empty and will be implemented in future commits.

---
+++ app/Filament/Pages/TestOpenAiPage.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+use Filament\Actions;
+
+class TestOpenAiPage extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.test-open-ai-page';
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\Action::make('Fetch Models')
+                ->action(function () {
+
+
+                })
+        ];
+    }
+}
