c03558c34d38ce09fbf43ad30d5432a894d76723
Chris Reed <chris@reedtech.us>
2023-11-16 20:59:12 -0600

    Update OpenAI facade usage and actions
    Refactor facade use statement to use OpenAI instead of OpenAiClient. Update 'Fetch Models' action to list all models. Add new 'List Assistants' action to list all assistants for debugging.

--- app/Filament/Pages/TestOpenAiPage.php
+++ app/Filament/Pages/TestOpenAiPage.php
@@ -2,7 +2,7 @@
 
 namespace App\Filament\Pages;
 
-use ChrisReedIO\OpenAI\SDK\Facades\OpenAiClient;
+use ChrisReedIO\OpenAI\SDK\Facades\OpenAI;
 use Filament\Pages\Page;
 use Filament\Actions;
 
@@ -10,15 +10,23 @@ class TestOpenAiPage extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
-    protected static string $view = 'openai-sdk::filament.pages.test-open-ai-page';
+    protected static string $view = 'filament.pages.test-open-ai-page';
+
+    public array $assistants = [];
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\Action::make('Fetch Models')
                 ->action(function () {
-                    $models = OpenAiClient::models()->list();
+                    $models = OpenAI::models()->list()->all();
                     dd($models);
+                }),
+            Actions\Action::make('List Assistants')
+                ->action(function () {
+                    $assistants = OpenAI::assistants()->list()->collect()->all();
+                    dd($assistants);
+
                 })
         ];
     }
