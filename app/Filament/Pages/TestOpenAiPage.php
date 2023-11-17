b6af9a79d0e9ae42a8ab2fd576acb39e6b7a9af5
Chris Reed <chris@reedtech.us>
2023-11-16 18:09:54 -0600

    Integrate OpenAI SDK with Filament page
    Added OpenAI API key to .env.example. Integrated OpenAI Client in TestOpenAiPage's Fetch Models action. Also updated composer.json to include local path for Laravel-OpenAI-SDK.

--- app/Filament/Pages/TestOpenAiPage.php
+++ app/Filament/Pages/TestOpenAiPage.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages;
 
+use ChrisReedIO\OpenAI\SDK\Facades\OpenAiClient;
 use Filament\Pages\Page;
 use Filament\Actions;
 
@@ -9,14 +10,15 @@ class TestOpenAiPage extends Page
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
-    protected static string $view = 'filament.pages.test-open-ai-page';
+    protected static string $view = 'openai-sdk::filament.pages.test-open-ai-page';
 
     protected function getHeaderActions(): array
     {
         return [
             Actions\Action::make('Fetch Models')
                 ->action(function () {
-
+                    $models = OpenAiClient::models()->list();
+                    dd($models);
                 })
         ];
     }
